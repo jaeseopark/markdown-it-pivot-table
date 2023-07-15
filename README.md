948553ed2448dbae1a75697b8ff799124263bbbd
Jaeseo Park <20038316+jaeseopark@users.noreply.github.com>
2023-07-14 21:27:52 -0600

    feat: aggregation

--- README.md
+++ README.md
@@ -6,22 +6,21 @@ Example
 
 aaaaaaa foobar.
 
-<BEGIN SOURCE --render=false>
 
-|Category|Sub-category|Item|Unit Cost|Qty|
-|---|---|---|---|---|
-|Electrical|Wires|14/2 (/m)|2|75|
-|Electrical|Wires|10/3 (/m)|9.98|10|
-|Electrical|Devices|Breakers|29.97|3|
+|Group=Category|Group=Sub-category|Item|Unit Cost|Qty|Subtotal=SUM()|
+|---|---|---|---|---|---|
+|Electrical|Wires|14/2 (/m)|2|75|{Unit Cost}*{Qty}|
+|Electrical|Wires|10/3 (/m)|9.98|10|{Unit Cost}*{Qty}|
+|Electrical|Devices|Breakers|29.97|3|{Unit Cost}*{Qty}|
 
-<END SOURCE>
+|Group=Category|Group=Sub-category|Item|Unit Cost|Qty|Subtotal=SUM({Unit Cost}*{Qty})|
+|---|---|---|---|---|---|
+|Wall|Drywall|1/2" (/sheet)|17.35|25|
+|Wall|Mud|Quickset|15|2|
+|Wall|Mud|All Purpose|35|3|
 
-<BEGIN PIVOT>
-rows={Category},{Sub-category}
-value=SUM({Unit Cost}*{Qty})
-header=Value
-sort={Category} ASC,{Value} DESC
-<END PIVOT>
+
+Yes
 
 aaa
 
--- index.js
+++ index.js
@@ -1,64 +1,195 @@
-const RE_SOURCE = /^<BEGIN SOURCE --render=(.*)>$([\s\S]*)^<END SOURCE>$/;
-const RE_PIVOT = /^<BEGIN PIVOT(.*)>$([\s\S]*)^<END PIVOT>$/;
+const md = require("markdown-it")();
+const csvToMarkdown = require("csv-to-markdown-table");
+const groupBy = require("group-by");
+const stringMath = require("string-math");
 
-const usePivotTable = (md) => {
-  //md.inline.ruler.push
-  // md.block.ruler.push()
+const RE_VARIABLE_SUBSTITUTE = /{[^}]+}/g;
+const RE_AGGREGATED_VALUE = /^(SUM|AVG|MIN|MAX|CNT)\((.*)\)/;
 
-  md.inline.ruler.push("pivot_table", (state, silent) => {
-    const {
-      md: { inline },
-      src,
-    } = state;
-    let { pos, posMax } = state;
+const sum = (values) => values.reduce((a, b) => a + (b || 0), 0);
 
-    const match = state.src.match(RE_SOURCE);
+const AGGREGATORS = {
+  MIN: Math.min,
+  MAX: Math.max,
+  CNT: (values) => values.length,
+  SUM: sum,
+  AVG: (values) => sum(values) / values.length,
+};
 
-    while (pos < posMax) {
-      const match = src.match(RE_SOURCE);
-      if (match) {
-        console.log(match);
+const sliceByTokenTypes = (tokens, startType, endType) => {
+  const sliced = [];
+  for (let i = 0; i < tokens.length; i++) {
+    const token = tokens[i];
+    if (token.type === startType) {
+      for (let j = i; j < tokens.length; j++) {
+        const nextToken = tokens[j];
+        if (nextToken.type === endType) {
+          sliced.push(tokens.slice(i, j + 1));
+          i = j;
+          break;
+        }
       }
-      pos++;
+    }
+  }
+  return sliced;
+};
 
-      //   inline.skipToken(state);
+const toTable = (tokens) => {
+  let isPivotTable = false;
+  let aggregatedColumn = null;
+
+  const columns = sliceByTokenTypes(tokens, "th_open", "th_close").map(
+    (column_tokens, i, arr) => {
+      const isLastColumn = i === arr.length - 1;
+      const content = column_tokens[1].content;
+      const containsEqual = content.includes("=");
+
+      if (containsEqual) {
+        isPivotTable = true;
+        const [left, right] = content.split("="); // TODO: need error handling for multiple equal signs.
+
+        if (isLastColumn) {
+          const match = right.trim().match(RE_AGGREGATED_VALUE);
+          aggregatedColumn = {
+            index: i,
+            type: "aggregated",
+            name: left.trim(),
+            aggregator: match[1],
+            equation: match[2],
+          };
+          return aggregatedColumn;
+        }
+
+        return {
+          index: i,
+          type: left.trim().toLowerCase(),
+          name: right.trim(),
+        };
+      }
+
+      return {
+        index: i,
+        type: "regular",
+        name: content.trim(),
+      };
+    }
+  );
+
+  const tbodyTokens = sliceByTokenTypes(tokens, "tbody_open", "tbody_close")[0];
+  const rowTokens = sliceByTokenTypes(tbodyTokens, "tr_open", "tr_close");
+  const rows = rowTokens.map((rowToken) =>
+    sliceByTokenTypes(rowToken, "td_open", "td_close").flatMap(
+      (cellTokens) => cellTokens[1].content
+    )
+  );
+
+  if (!isPivotTable || !aggregatedColumn) {
+    return {
+      isPivotTable,
+    };
+  }
+
+  const columnMap = columns.reduce((acc, next) => {
+    acc[next.name] = next;
+    return acc;
+  }, {});
+  const visitedColumns = [];
+  const getCellValue = (row, column) => {
+    let cellValue = row[column.index] || column.equation;
+    if (cellValue) {
+      const match = cellValue.match(RE_VARIABLE_SUBSTITUTE);
+      if (match) {
+        match.forEach((columnName) => {
+          const sanitizedColumnName = columnName.replace(/[{}]/g, "");
+          if (!visitedColumns.includes(sanitizedColumnName)) {
+            const nextValue = getCellValue(row, columnMap[sanitizedColumnName]);
+            cellValue = cellValue.replace(columnName, nextValue);
+          }
+        });
+      }
     }
 
-    console.log(src);
+    row[column.index] = cellValue;
+    return cellValue;
+  };
 
-    // for (let idx = 1; idx < tokens.length; idx++) {
-    //   const token = tokens[idx];
-    //   const { type, content } = token;
-    //   if (
-    //     isSearchingSourceTable &&
-    //     type === "table_open" &&
-    //     lastInlineContent
-    //   ) {
-    //     const match = lastInlineContent.match(RE_BEGIN_SOURCE);
-    //     if (match) {
-    //       isDigestingSource = true;
-    //       shouldShowSourceTable = match[1].trim().toLowerCase() === "true";
-    //     }
-    //     isSearchingPivotKeyword = true;
-    //   }
-    //   if (type === "table_close" && isDigestingSource) {
-    //     isDigestingSource = false;
-    //   }
-    //   if (type === "inline") {
-    //     if (isDigestingSource) {
-    //       // TODO: digest source here
-    //     } else {
-    //       if (isSearchingPivotKeyword) {
-    //         const match = content.match(RE_PIVOT);
-    //         if (match) {
-    //           console.log(match);
-    //         }
-    //       } else {
-    //         lastInlineContent = content;
-    //       }
-    //     }
-    //   }
-    // }
+  rows.forEach((row) => {
+    for (let i = 0; i < columns.length; i++) {
+      getCellValue(row, columns[i]);
+    }
+  });
+
+  return {
+    isPivotTable,
+    columns,
+    visibleColumns: columns.filter((col) => col.type !== "regular"),
+    rows,
+    tokens: {
+      open: tokens[0],
+      close: tokens[tokens.length - 1],
+    },
+  };
+};
+
+const generateTableTokens = (table) => {
+  const aggregate = (rows) =>
+    table.visibleColumns.map((column) => {
+      if (column.type === "group") {
+        // return the cell value of the first row
+        return rows[0][column.index];
+      } else if (column.type === "aggregated") {
+        const individualValues = rows.map((row) => {
+          const cellValue = row[column.index];
+          if (cellValue) {
+            return stringMath(cellValue);
+          }
+        });
+        return AGGREGATORS[column.aggregator](individualValues);
+      }
+    });
+
+  const groups = Object.values(
+    groupBy(table.rows, (row) =>
+      table.visibleColumns
+        .filter((column) => column.type === "group")
+        .map((column) => row[column.index])
+        .join(":")
+    )
+  );
+
+  const aggregatedGroups = groups
+    .map(aggregate)
+    .map((aggregatedRow) => aggregatedRow.join(","));
+
+  const csv = [
+    table.visibleColumns.map((col) => col.name).join(","), // header
+    ...aggregatedGroups, // groups
+  ];
+
+  const stringified = csvToMarkdown(csv.join("\n"), ",", true);
+  const tokens = md.parse(stringified);
+  return tokens;
+};
+
+const update = (tokens, table) => {
+  const newTokens = generateTableTokens(table);
+  const openTokenIndex = tokens.findIndex(
+    (token) => token === table.tokens.open
+  );
+  const closeTokenIndex = tokens.findIndex(
+    (token) => token === table.tokens.close
+  );
+  const deleteCount = closeTokenIndex - openTokenIndex + 1;
+  tokens.splice(openTokenIndex, deleteCount, ...newTokens);
+};
+
+const usePivotTable = (md) => {
+  md.core.ruler.push("pivot_table", (state) => {
+    const { tokens } = state;
+    sliceByTokenTypes(tokens, "table_open", "table_close")
+      .map(toTable)
+      .filter((table) => table.isPivotTable)
+      .forEach((table) => update(tokens, table));
   });
 };
 
