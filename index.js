baed6cf51d8008e49105108bf436ef62da61e69f
Jaeseo Park <20038316+jaeseopark@users.noreply.github.com>
2023-08-15 16:22:53 -0600

    fix: check if the right side exists

--- index.js
+++ index.js
@@ -47,16 +47,18 @@ const toTable = (tokens) => {
         isPivotTable = true;
         const [left, right] = content.split("="); // TODO: need error handling for multiple equal signs.
 
-        const match = right.trim().match(RE_AGGREGATED_VALUE);
-        if (match) {
-          foundAggregatedColumn = true;
-          return {
-            index: i,
-            type: "aggregated",
-            name: left.trim(),
-            aggregator: match[1],
-            equation: match[2],
-          };
+        if (right) {
+          const match = right.trim().match(RE_AGGREGATED_VALUE);
+          if (match) {
+            foundAggregatedColumn = true;
+            return {
+              index: i,
+              type: "aggregated",
+              name: left.trim(),
+              aggregator: match[1],
+              equation: match[2],
+            };
+          }
         }
 
         return {
